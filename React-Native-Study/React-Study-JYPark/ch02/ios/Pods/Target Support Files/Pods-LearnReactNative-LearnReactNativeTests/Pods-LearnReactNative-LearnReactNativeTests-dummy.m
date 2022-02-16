70862c353a9191fcae38f85491540bed50129fd8
ParkJinYong <mej1406@gmail.com>
2022-02-16 18:33:12 +0900

    change dir ch02

---
+++ React-Native-Study/React-Study-JYPark/ch02/ios/Pods/Target Support Files/Pods-LearnReactNative-LearnReactNativeTests/Pods-LearnReactNative-LearnReactNativeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LearnReactNative_LearnReactNativeTests : NSObject
+@end
+@implementation PodsDummy_Pods_LearnReactNative_LearnReactNativeTests
+@end
